9776cea2659bcbe5b507e00f741a15f783badd65
TedPham397 <thinhphamart@gmail.com>
2024-04-03 18:37:16 -0700

    commit

---
+++ client/src/App.js
@@ -0,0 +1,48 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="" >Login</a>
+          <a href="" >Register</a>
+
+        </nav>
+      </header>
+
+        <div classname="post">
+          <div classname="image">
+          <img src="https://realpython.com/cdn-cgi/image/width=960,format=auto/https://files.realpython.com/media/NumPy-Random-Number-Generator_Watermarked.f87659b8e3ee.jpg" alt="" />
+          </div>
+          <div classname="texts">
+          <h2>Using the NumPy Random Number Generator</h2>
+          <p>Random numbers are a very useful feature in many different types of programs, from mathematics and data analysis through to computer games and encryption applications. You may be surprised to learn that it’s actually quite difficult to get a computer to generate true randomness. However, if you’re careful, the NumPy random number generator can generate random enough numbers for everyday purposes</p>
+          </div>
+          </div>
+
+         <div classname="post">
+          <div classname="image">
+          <img src="https://realpython.com/cdn-cgi/image/width=960,format=auto/https://files.realpython.com/media/NumPy-Random-Number-Generator_Watermarked.f87659b8e3ee.jpg" alt="" />
+          </div>
+          <div classname="texts">
+          <h2>Using the NumPy Random Number Generator</h2>
+          <p>Random numbers are a very useful feature in many different types of programs, from mathematics and data analysis through to computer games and encryption applications. You may be surprised to learn that it’s actually quite difficult to get a computer to generate true randomness. However, if you’re careful, the NumPy random number generator can generate random enough numbers for everyday purposes</p>
+          </div>
+          </div>
+
+          <div classname="post">
+          <div classname="image">
+          <img src="https://realpython.com/cdn-cgi/image/width=960,format=auto/https://files.realpython.com/media/NumPy-Random-Number-Generator_Watermarked.f87659b8e3ee.jpg" alt="" />
+          </div>
+          <div classname="texts">
+          <h2>Using the NumPy Random Number Generator</h2>
+          <p>Random numbers are a very useful feature in many different types of programs, from mathematics and data analysis through to computer games and encryption applications. You may be surprised to learn that it’s actually quite difficult to get a computer to generate true randomness. However, if you’re careful, the NumPy random number generator can generate random enough numbers for everyday purposes</p>
+          </div>
+          </div>
+    </main>
+
+  );
+}
+export default App;
