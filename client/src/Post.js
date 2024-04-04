32143be45f79a6c0182216456c414d96dece9fc3
TedPham397 <thinhphamart@gmail.com>
2024-04-04 13:21:56 -0700

    completed 1

--- client/src/Post.js
+++ client/src/Post.js
@@ -1,17 +1,25 @@
-export default function Post(){
-    return (
+import {formatISO9075} from "date-fns";
+import {Link} from "react-router-dom";
+
+export default function Post({_id,title,summary,cover,content,createdAt,author}) {
+
+  return (
     <div className="post">
-          <div className="image">
-          <img src="https://realpython.com/cdn-cgi/image/width=960,format=auto/https://files.realpython.com/media/NumPy-Random-Number-Generator_Watermarked.f87659b8e3ee.jpg" alt="" />
-          </div>
-          <div className="texts">
-          <h2>Using the NumPy Random Number Generator</h2>
-          <p className="info" >
-            <a className="author">Ted Pham</a>
-            <time>2024-04-03 6:39</time>
-          </p>
-          <p className="summary">Random numbers are a very useful feature in many different types of programs, from mathematics and data analysis through to computer games and encryption applications. You may be surprised to learn that it’s actually quite difficult to get a computer to generate true randomness. However, if you’re careful, the NumPy random number generator can generate random enough numbers for everyday purposes</p>
-          </div>
-          </div>
-    )
+      <div className="image">
+        <Link to={`/post/${_id}`}>
+          <img src={'http://localhost:4000/'+cover} alt=""/>
+        </Link>
+      </div>
+      <div className="texts">
+        <Link to={`/post/${_id}`}>
+        <h2>{title}</h2>
+        </Link>
+        <p className="info">
+          <a className="author">{author.username}</a>
+          <time>{formatISO9075(new Date(createdAt))}</time>
+        </p>
+        <p className="summary">{summary}</p>
+      </div>
+    </div>
+  );
 }
